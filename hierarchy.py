78301e86dd2e9a481a92cee6c29d7791a4e769dd
dacoder <107454678+dacoder101@users.noreply.github.com>
2023-04-11 18:15:36 -0700

    Release 3
    Release 3

--- hierarchy.py
+++ hierarchy.py
@@ -17,15 +17,13 @@ from console import fx
 
 # Setup
 
-hierarchy = ""
-
 
 def cls():
     sys('clear')
 
 
 if "HierarchyCreator" not in lsDir():
-    mkDir("HierarchyCreator")
+    mkDir("HierarchyCreator/")
 
 # Program
 
@@ -39,7 +37,7 @@ while True:
         o = m.selector()
         if not o: pass
         else:
-            h = Hierarchy(o)
+            h = Hierarchy(o, f"{o}")
             m.creator(h)
     elif o == "i":
         m.info()
