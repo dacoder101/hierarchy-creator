97f4be69cb31c93f4c966ab2ef2dd681eefa4296
dacoder <107454678+dacoder101@users.noreply.github.com>
2023-04-14 22:37:32 -0700

    Release 5, Imports & __name__ == "__main__"
    Release 5, Imports & __name__ == "__main__"

--- hierarchy.py
+++ hierarchy.py
@@ -4,16 +4,20 @@
 # Imports
 
 from func import Menu as m
+from func import Command as c
 from func import Function as f
 from func import Hierarchy
 
 from os import listdir as lsDir
 from os import mkdir as mkDir
+from os import remove as rmFile
 from os import system as sys
 import os
 
 from console.utils import wait_key as key
 from console import fx
+
+from shutil import rmtree as rmDir
 
 # Setup
 
@@ -27,26 +31,31 @@ if "HierarchyCreator" not in lsDir():
 
 # Program
 
-m.title()
-while True:
-    o = m.mainMenu()
-    if o == "": pass
-    elif o == "c":
-        m.create()
-    elif o == "o":
-        o = m.selector()
-        if not o: pass
+
+def main():
+    m.title()
+    while True:
+        o = m.mainMenu()
+        if o == "": pass
+        elif o == "c":
+            m.create()
+        elif o == "o":
+            o = m.selector()
+            if not o or o == None: pass
+            else:
+                h = Hierarchy(o, o)
+                m.creator(h)
+        elif o == "t":
+            m.tutorial()
+        elif o == "i":
+            m.info()
+        elif o == "e":
+            cls()
+            break
         else:
-            h = Hierarchy(o, f"{o}")
-            m.creator(h)
-    elif o == "t":
-        m.tutorial()
-    elif o == "i":
-        m.info()
-    elif o == "e":
-        cls()
-        break
-    else:
-        cls()
-        print(m.error(f"Option \"{o}\" not found."))
-        key()
+            cls()
+            print(m.error(f"The option \"{o}\" has not been found."))
+            key()
+
+
+if __name__ == "__main__": main()
